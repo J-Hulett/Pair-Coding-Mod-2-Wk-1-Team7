3533a644d6b0be531c1eb6def3b616b8d16c04a2
Jake Hall <JakeHall1218@yahoo.com>
2022-04-09 15:18:24 -0400

    Pushing module 2 week 1 paired exercises

--- exercise/step-4.sql
+++ exercise/step-4.sql
@@ -1,11 +1,23 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
-
+SELECT park.name, campground.name, open_from_mm, open_to_mm, daily_fee
+FROM park
+JOIN campground ON park.park_id = campground.park_id
+ORDER BY park.name, campground.name;
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
-
+SELECT park.name, COUNT(campground_id) AS num_of_campgrounds
+FROM park
+JOIN campground ON park.park_id = campground.park_id
+GROUP BY park.name
+ORDER BY park.name;
 
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
+SELECT park.name, campground.name, site_number, max_occupancy, accessible, max_rv_length, utilities
+FROM park
+JOIN campground ON park.park_id = campground.park_id
+JOIN site ON campground.campground_id = site.campground_id
+WHERE campground.name = 'Blackwoods';
 
 /*
   select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
@@ -19,10 +31,18 @@
     Cuyahoga Valley	The Unnamed Primitive Campsites	5
   -------------------------------------------------
 */
-
+SELECT park.name, campground.name, COUNT(site_id) AS number_of_sites
+FROM park
+JOIN campground ON park.park_id = campground.park_id
+JOIN site ON campground.campground_id = site.campground_id
+GROUP BY park.name, campground.name
+ORDER BY park.name ASC;
 
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
-
+SELECT site_number, reservation.name, from_date, to_date
+FROM site
+JOIN reservation ON site.site_id = reservation.site_id
+ORDER BY from_date ASC;
 
 
 /*
@@ -37,6 +57,12 @@
     Juniper Group Site	4
   -------------------------------------------------
 */
+SELECT campground.name, COUNT(reservation_id) AS num_reservations
+FROM campground
+JOIN site ON campground.campground_id = site.campground_id
+JOIN reservation ON site.site_id = reservation.site_id
+GROUP BY campground.name
+ORDER BY num_reservations DESC;
 
 
 
--- exercise/step-5.sql
+++ exercise/step-5.sql
@@ -9,12 +9,18 @@
   description: Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.
   ------------------------------
 */
+INSERT INTO park(name, location, establish_date, area, visitors, description)
+VALUES ('Ohiopyle State Park', 'Pennsylvania', '1965-01-01', 19052, 1000000
+, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.');
 
-
+SELECT * FROM park;
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
-
 */
+UPDATE park SET visitors = 1500000
+WHERE park_id = 4;
+
+SELECT * FROM park;
 
 
 /*
@@ -27,7 +33,11 @@
   daily_fee: 95.00
   ------------------------------------------------------------
 */
+INSERT INTO campground(park_id, name, open_from_mm, open_to_mm, daily_fee)
+VALUES (4, 'Youghiogheny', '01', '12', '95.00')
 
+SELECT * FROM campground
+WHERE park_id = 4;
 
 /*
  STEP FOUR: Insert 3 new sites with the following data:
@@ -40,7 +50,13 @@
  > campground_id 8 should be the id of the campground you just added 'Youghiogheny'
 
 */
+INSERT INTO site (site_number, campground_id)
+VALUES (623, 8)
+, (624, 8)
+, (625, 8);
 
+SELECT * FROM site
+WHERE campground_id = 8;
 
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
@@ -51,16 +67,38 @@
 ------------------------------------------------------------------------------------
 
 */
+BEGIN TRANSACTION;
+INSERT INTO reservation(site_id, name, from_date, to_date)
+VALUES (623, 'Wayne Family', '2022-04-19' , '2022-04-29')
+, (624, 'Parker Family', '2022-04-20' , '2022-04-29')
+, (625, 'Kent Family', '2022-04-21', '2022-04-29')
+COMMIT;
+
+SELECT * FROM reservation;
 
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
+ UPDATE reservation SET from_date = '2022-04-09'
+ WHERE reservation_id = (SELECT reservation_id FROM reservation WHERE name = 'Wayne Family');
+ UPDATE reservation SET to_date = '2022-04-16'
+ WHERE reservation_id = (SELECT reservation_id FROM reservation WHERE name = 'Wayne Family');
+ 
+ SELECT * FROM reservation;
+ 
 
 
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
 */
+DELETE FROM reservation 
+WHERE reservation_id = (SELECT reservation_id FROM reservation WHERE name = 'Kent Family');
+
+SELECT * FROM reservation;
+
+
+SELECT * FROM reservation WHERE name = 'Kent Family';
 
